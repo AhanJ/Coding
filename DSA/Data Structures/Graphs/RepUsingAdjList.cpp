25ebcb6ccb5a6625acab3dd8e65ebcab98e20aef
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2023-09-01 00:19:02 +0530

    Update RepUsingAdjList.cpp

--- DSA/Data Structures/Graphs/RepUsingAdjList.cpp
+++ DSA/Data Structures/Graphs/RepUsingAdjList.cpp
@@ -17,16 +17,33 @@ int main()
 
     vector<int> adj[u + 1]; // to initialise an array we use '<datatype> arr[size]'. What this does is create an array which can store data of type specified. Here what we do is create an array of size one greater than the number of nodes we have (assuming nodes start from 1 i.e. base 1) and each cell in this array stores a vector of type 'int'. We create an array of integer vectors. This is essentially the implementation of a list.
 
+    cout << "Enter the Nodes for Connecting (Separated by a Space)...\n\n";
+
     for (int i = 0; i < v; i++) // i < number of edges because number of edges represent number of connections
 
     {
         int u1, u2;
 
-        cout << "Enter the Nodes for Connecting (Separated by a Space): ";
         cin >> u1 >> u2;
 
         adj[u1].push_back(u2); // push u2 in the vector at index u1
         adj[u2].push_back(u1); // push u1 in the vector at index u2, not necessary for directed graph
+    }
+
+    cout << endl;
+
+    for (int i = 1; i < u + 1; i++)
+
+    {
+        cout << "Node " << i << " -> ";
+
+        for (auto x : adj[i])
+
+        {
+            cout << " | " << x;
+        }
+
+        cout << " | " << endl;
     }
 
     return 0;
